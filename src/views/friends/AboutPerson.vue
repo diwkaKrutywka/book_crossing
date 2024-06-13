8a6e67bad1535a9e5f41f05ac400a5d7b3cbbc83
Diana Omarbayeva <diwka.omarbayeva8@gmail.com>
2024-06-13 10:34:24 +0600

    ---

--- src/views/friends/AboutPerson.vue
+++ src/views/friends/AboutPerson.vue
@@ -4,7 +4,7 @@
     <div class="card">
       <div class="friend-info">
         <div class="friend-profile">
-          <img src="@/assets/images/person.png" />
+          <img src="@/assets/images/person.png" style="width: 70px" />
           <h4>{{ info.username }}</h4>
           <h4>{{ info.email }}</h4>
           <button @click="addFriend()" v-if="info.friend_status == 'not_friends'" class="btn">
